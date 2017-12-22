95dc36ecb6c928b9416f5ef3b87125bb9334935a
Jeremy Dahl <32681183+remydahl@users.noreply.github.com>
2017-12-22 08:14:42 -0800

    Add files via upload

---
+++ Create-AdminCredentials.ps1
@@ -0,0 +1,11 @@
+﻿
+$credpath = ".\O365Credential.xml"
+$admAccount = "user@domain.com"
+$admPassword = "admpassword"
+
+New-Object System.Management.Automation.PSCredential($adminAccount, (ConvertTo-SecureString -AsPlainText -Force $admPassword)) | Export-CliXml $credpath 
+
+$credFilePath = ".\O365Credential.xml" #xml file that holds the global admin login information
+ 
+#Login credential to Office 365 with admin rights
+$credential = Import-Clixml -Path $credFilePath 
