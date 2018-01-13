f1381eb0787f96ac8d855b754afa097b64ca87f9
Jeremy Dahl <32681183+remydahl@users.noreply.github.com>
2018-01-12 21:13:11 -0800

    Add files via upload

---
+++ Connect-SFB-Online.ps1
@@ -0,0 +1,12 @@
+﻿$credential = Get-Credential
+Connect-MsolService -Credential $credential
+
+# Find the root (onmicrosoft.com) tenant domain
+Write-Host "Connected to MS Online Services, checking admin domain..." -ForegroundColor Yellow
+$msolDomain = Get-MsolDomain | where {$_.Name -match "onmicrosoft.com" -and $_.Name -notmatch "mail.onmicrosoft.com"}
+
+Write-Host "Admin domain found, connecting to $($msolDomain.Name)" -ForegroundColor Green
+
+# Use this domain to connect to SFB Admin domain
+$session = New-CsOnlineSession -Credential $credential -OverrideAdminDomain $msolDomain.Name
+Import-PSSession $session
